82049931f9f18347fe79003535365e1c5f9dc5ec
AstghikTonoyan <tonoyan1994@gmail.com>
2023-06-30 11:55:08 +0400

    some change

--- ctblog/ctblog/settings.py
+++ ctblog/ctblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@5-00_198b^r@^v@_tb2n7t(fa(&xc-kls%&2bp!h@fjzmtm&q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
